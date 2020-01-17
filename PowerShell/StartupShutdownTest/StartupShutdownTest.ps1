f07ee5e0a887325eb950a9b26b5aa2d575965cba
kohei_ishikawa <kohei_ishikawa@iwi.co.jp>
2020-01-17 10:45:32 +0900

    イベントログよりPC起動、シャットダウン関連のイベントを取得してコンソール出力するスクリプト追加

---
+++ PowerShell/StartupShutdownTest/StartupShutdownTest.ps1
@@ -0,0 +1,2 @@
+﻿#イベントログよりPC起動、シャットダウン関連のイベントを取得してコンソール出力する
+GET-WinEvent System | Where-Object{$_.Id -eq 6005 -or $_.Id -eq 6006 -or $_.Id -eq 6008 -or $_.Id -eq 7002 -or $_.Id -eq 7001} | select-Object TimeCreated,Id,Message | Format-Table *
